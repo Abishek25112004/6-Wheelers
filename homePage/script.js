70883b7302024e38bcc49ee76dfc99bb253c3480
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-20 14:54:40 +0530

    completed login page

--- homePage/script.js
+++ homePage/script.js
@@ -1,5 +1,16 @@
 let loginBtn = document.querySelector('.loginBtn')
+let loginOutline = document.querySelector('.login-outline');
+let overlay = document.querySelector('.overlay')
 
 loginBtn.addEventListener('click',()=>{
-   document.querySelector('.login-outline').style.display = 'block'
+   loginOutline.style.display = 'block';
+   overlay.style.display = 'block';
+})
+
+let xBtn = document.querySelector('.x-btn');
+
+xBtn.addEventListener('click',()=>{
+    loginOutline.style.display = "none";
+    overlay.style.display = "none";
+
 })
