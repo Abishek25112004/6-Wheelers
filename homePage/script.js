5d2374797382da25789eddd97757f4a37cfe6838
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-20 19:16:49 +0530

    created forget password page and styled

--- homePage/script.js
+++ homePage/script.js
@@ -1,16 +1,48 @@
+//    Login display
+
 let loginBtn = document.querySelector('.loginBtn')
 let loginOutline = document.querySelector('.login-outline');
 let overlay = document.querySelector('.overlay')
+let body = document.querySelector('.body')
 
 loginBtn.addEventListener('click',()=>{
-   loginOutline.style.display = 'block';
+   loginOutline.classList.add("login-outline-display");
    overlay.style.display = 'block';
+   body.classList.add("scroll");
 })
 
 let xBtn = document.querySelector('.x-btn');
 
 xBtn.addEventListener('click',()=>{
-    loginOutline.style.display = "none";
+    loginOutline.classList.remove("login-outline-display");
     overlay.style.display = "none";
+    body.classList.remove("scroll")
+})
+
+//  Forget Password displayfor
+
+let forgetPassBtn = document.querySelector('.forgetPass-btn-in-signin')
+let forgetOutline = document.querySelector('.forget-outline')
+
+forgetPassBtn.addEventListener('click',()=>{
+    forgetOutline.classList.add("forget-outline-display");
+    overlay.style.display = 'block';
+    loginOutline.classList.remove("login-outline-display");
 
 })
+
+let forgetXBtn = document.querySelector('.forget-x-btn');
+
+forgetXBtn.addEventListener('click',()=>{
+    forgetOutline.classList.remove("forget-outline-display");
+    overlay.style.display = 'none';
+})
+
+let forgetBackToLogin = document.querySelector('.forget-back-to-login')
+
+forgetBackToLogin.addEventListener('click',()=>{
+    forgetOutline.classList.remove("forget-outline-display");
+    loginOutline.classList.add("login-outline-display");
+})
+
+
