510273ec09e3b1cc88083387f26d85f12a6d5062
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-21 14:48:19 +0530

    created OTP verification in HTML and styled in CSS

--- homePage/script.js
+++ homePage/script.js
@@ -19,7 +19,7 @@ xBtn.addEventListener('click',()=>{
     body.classList.remove("scroll")
 })
 
-//  Forget Password displayfor
+//  Forget Password display
 
 let forgetPassBtn = document.querySelector('.forgetPass-btn-in-signin')
 let forgetOutline = document.querySelector('.forget-outline')
@@ -36,6 +36,7 @@ let forgetXBtn = document.querySelector('.forget-x-btn');
 forgetXBtn.addEventListener('click',()=>{
     forgetOutline.classList.remove("forget-outline-display");
     overlay.style.display = 'none';
+    body.classList.remove("scroll")
 })
 
 let forgetBackToLogin = document.querySelector('.forget-back-to-login')
@@ -45,4 +46,75 @@ forgetBackToLogin.addEventListener('click',()=>{
     loginOutline.classList.add("login-outline-display");
 })
 
+let forgetBackBtn = document.querySelector('.forget-back-btn')
+
+forgetBackBtn.addEventListener('click',()=>{
+    forgetOutline.classList.remove("forget-outline-display");
+    loginOutline.classList.add("login-outline-display");
+})
+
+//    OTP Verification
+
+let resetBtn =  document.querySelector('.reset-btn')
+let OtpOutline = document.querySelector('.OTP-outline')
+
+resetBtn.addEventListener('click',()=>{
+   OtpOutline.style.display = "block"
+   forgetOutline.classList.remove("forget-outline-display");
+   overlay.style.display = 'block';
+})
+
+let OtpXBtn = document.querySelector('.OTP-x-btn')
+
+OtpXBtn.addEventListener('click',()=>{
+    OtpOutline.style.display = "none"
+    overlay.style.display = 'none';
+    body.classList.remove("scroll")
+})
+
+let OptBackToLogin = document.querySelector('.OTP-back-to-login')
+
+OptBackToLogin.addEventListener('click',()=>{
+    OtpOutline.style.display = "none"
+    loginOutline.classList.add("login-outline-display");
+})
+
+let OtpBackBtn = document.querySelector('.OTP-back-btn')
+
+OtpBackBtn.addEventListener('click',()=>{
+    OtpOutline.style.display = "none"
+    forgetOutline.classList.add("forget-outline-display");
+})
+
+//     Confirm password
+
+let OtpContinueBtn = document.querySelector('.OTP-continue-btn')
+let setPassOutline = document.querySelector('.set-pass-outline')
+
+OtpContinueBtn.addEventListener('click',()=>{
+    setPassOutline.classList.add("set-pass-outline-display");
+    overlay.style.display = 'block';
+     OtpOutline.style.display = "none"
+})
+
+let setPassXBtn = document.querySelector('.set-pass-x-btn')
+
+setPassXBtn.addEventListener('click',()=>{
+    setPassOutline.classList.remove("set-pass-outline-display");
+    overlay.style.display = 'none';
+})
+
+let setPassBackToLoginBtn = document.querySelector('.set-pass-back-to-login-btn')
+
+setPassBackToLoginBtn.addEventListener('click',()=>{
+    setPassOutline.classList.remove("set-pass-outline-display");
+    loginOutline.classList.add("login-outline-display");
+})
+
+let setPassBackBtn = document.querySelector('.set-pass-back-btn')
+
+setPassBackBtn.addEventListener('click',()=>{
+    setPassOutline.classList.remove("set-pass-outline-display");
+     OtpOutline.style.display = "block"
+})
 
